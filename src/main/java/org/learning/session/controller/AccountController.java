f0416ab014f4a79fabb53e2ffdf856653707fe17
aveasura <aveasura@gmail.com>
2025-01-10 20:09:33 +0300

    [refactor] AccountController to use Spring

--- src/main/java/org/learning/session/controller/AccountController.java
+++ src/main/java/org/learning/session/controller/AccountController.java
@@ -1,19 +1,13 @@
 package org.learning.session.controller;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
-import java.io.IOException;
+@Controller
+public class AccountController {
 
-@WebServlet("/account")
-public class AccountController extends HttpServlet {
-    private static final String ACCOUNT_PAGE = "/WEB-INF/home/auth/account/account.jsp";
-
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher(ACCOUNT_PAGE).forward(req, resp);
+    @GetMapping("/account")
+    private String accountPage() {
+        return "/auth/account/account";
     }
 }

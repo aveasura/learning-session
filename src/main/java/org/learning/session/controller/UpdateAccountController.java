33f178ccc3bdbbdabf041a79a7dab36f89c2e0ed
aveasura <aveasura@gmail.com>
2025-01-10 21:48:15 +0300

    [refactor] UpdateAccountController to use spring

--- src/main/java/org/learning/session/controller/UpdateAccountController.java
+++ src/main/java/org/learning/session/controller/UpdateAccountController.java
@@ -1,56 +1,47 @@
 package org.learning.session.controller;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import org.learning.session.service.UserService;
 import org.learning.session.service.ValidationException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
+@Controller
+@RequestMapping("/account")
+@SessionAttributes("userId")  // атрибут, который будет храниться в сессии
+public class UpdateAccountController {
 
-@WebServlet("/account/update")
-public class UpdateAccountController extends HttpServlet {
-    private static final String UPDATE_USER_PAGE = "/WEB-INF/home/auth/update.jsp";
-    private static final String UPDATE_REDIRECT = "/account/update";
-    private static final String ACCOUNT_REDIRECT = "/account";
+    private final UserService userService;
 
-    private UserService userService;
-
-    @Override
-    public void init() throws ServletException {
-        userService = (UserService) getServletContext().getAttribute("userService");
+    public UpdateAccountController(UserService userService) {
+        this.userService = userService;
     }
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher(UPDATE_USER_PAGE).forward(req, resp);
+    @GetMapping("/update")
+    private String updatePage() {
+        return "/auth/update";
     }
 
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String username = req.getParameter("username");
-        String password = req.getParameter("password");
-
-        HttpSession session = req.getSession();
-        long userId = (long) session.getAttribute("userId");
-
+    // используем @ModelAttribute для извлечения userId из сессии
+    @PostMapping("/update")
+    private String doUpdate(@RequestParam String username,
+                            @RequestParam String password,
+                            @ModelAttribute("userId") Long userId,
+                            HttpSession session,
+                            Model model) {
         try {
             userService.updateUserById(userId, username, password);
             session.setAttribute("username", username);
-            redirect(req, resp, ACCOUNT_REDIRECT, "Профиль успешно обновлён");
+
+            // tmp
+            model.addAttribute("success", "Пользователь свои данные. ID: " + userId);
+
+            return "redirect:/account";
         } catch (ValidationException e) {
-            // redirect(req, resp, UPDATE_REDIRECT, e.getMessage());
-
-            req.setAttribute("error", e.getMessage());
-            req.getRequestDispatcher(UPDATE_USER_PAGE).forward(req, resp);
+            model.addAttribute("error", e.getMessage());
+            return "/auth/update";
         }
-    }
 
-    private void redirect(HttpServletRequest req, HttpServletResponse resp, String path, String message) throws IOException {
-        System.out.println(message);
-        resp.sendRedirect(req.getContextPath() + path);
     }
 }
